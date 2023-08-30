207dec28f5b3f75656a5d41f900e5f70ef6afe43
yanad <yana.danilova1509@gmail.com>
2023-08-30 14:25:06 +0200

    spring security step 58-59

---
+++ src/main/java/com/springboot/in28minuten/configurations/SpringSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.springboot.in28minuten.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //		1) All requests should be authenticated
+        http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+        //		2) If a request is not authenticated, a web page is shown
+        http.httpBasic(Customizer.withDefaults());
+        //		3) CSRF -> POST, PUT
+        http.csrf().disable();
+        return http.build();
+    }
+}
