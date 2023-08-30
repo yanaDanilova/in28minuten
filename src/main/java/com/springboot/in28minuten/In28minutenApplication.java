60561367d613168b2101018d4356b3298813c124
yanad <yana.danilova1509@gmail.com>
2023-08-30 09:48:51 +0200

    Initial commit

---
+++ src/main/java/com/springboot/in28minuten/In28minutenApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.in28minuten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minutenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28minutenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/in28minuten/In28minutenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.in28minuten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28minutenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
